93e0d79fad604f7b0494f738f65cd5c6c46ecc8c
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2009-08-23 22:51:02 +0100

    Fix copyright date

--- src/empathy-import-mc4-accounts.h
+++ src/empathy-import-mc4-accounts.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008 Collabora Ltd.
+ * Copyright (C) 2009 Collabora Ltd.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
