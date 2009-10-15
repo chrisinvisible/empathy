6703efaad20fd368471e43dc9eeed272f848e39a
Cosimo Cecchi <cosimo.cecchi@collabora.co.uk>
2009-10-15 13:43:04 +0200

    Use new API in the test.

--- tests/test-empathy-account-assistant.c
+++ tests/test-empathy-account-assistant.c
@@ -12,7 +12,7 @@ int main (int argc, char **argv)
   gtk_init (&argc, &argv);
   empathy_gtk_init ();
 
-  assistant = empathy_account_assistant_new (NULL);
+  assistant = empathy_account_assistant_show (NULL);
 
   gtk_widget_show_all (assistant);
 
