3fbe87fc0d91b765ac55c1ad683e328af446110b
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-12-17 10:38:43 +0000

    contact-selector-dialog: set a border of 12 as defined in the HIG

--- libempathy-gtk/empathy-contact-selector-dialog.c
+++ libempathy-gtk/empathy-contact-selector-dialog.c
@@ -282,6 +282,8 @@ empathy_contact_selector_dialog_init (EmpathyContactSelectorDialog *dialog)
   gtk_window_set_position (GTK_WINDOW (dialog), GTK_WIN_POS_CENTER_ON_PARENT);
   gtk_window_set_type_hint (GTK_WINDOW (dialog), GDK_WINDOW_TYPE_HINT_DIALOG);
 
+  gtk_container_set_border_width (GTK_CONTAINER (dialog), 12);
+
   /* text completion */
   completion = gtk_entry_completion_new ();
   model = gtk_list_store_new (3, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_STRING);
