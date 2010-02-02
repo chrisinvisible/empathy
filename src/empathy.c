47ce699f4a9b8bd726330c39995f508fa819b85d
Danielle Madeley <danielle.madeley@collabora.co.uk>
2010-02-02 14:24:42 +1100

    empathy.c: Initialise variable (#609030)
    Fixes bug introduced in 24db41f217eea1cbf929cc3e623c50ca626db4b

--- src/empathy.c
+++ src/empathy.c
@@ -374,8 +374,12 @@ do_show_accounts_ui (GtkWindow *window,
     {
       EmpathyConnectionManagers *cm_mgr;
 
+      cm_mgr = empathy_connection_managers_dup_singleton ();
+
       empathy_connection_managers_prepare_async (cm_mgr,
           connection_managers_prepare_for_accounts, NULL);
+
+      g_object_unref (cm_mgr);
     }
 }
 
