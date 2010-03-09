9a912cfc5dfe0592d4e04c7da517383b49c99b95
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2010-03-09 11:59:07 +0100

    remove EMPATHY_IMAGE_(UN)FAVOURITE

--- libempathy-gtk/empathy-images.h
+++ libempathy-gtk/empathy-images.h
@@ -41,8 +41,6 @@ G_BEGIN_DECLS
 #define EMPATHY_IMAGE_VIDEO_CALL          "camera-web"
 #define EMPATHY_IMAGE_LOG                 "document-open-recent"
 #define EMPATHY_IMAGE_DOCUMENT_SEND       "document-send"
-#define EMPATHY_IMAGE_FAVOURITE           "empathy-starred"
-#define EMPATHY_IMAGE_UNFAVOURITE         "empathy-unstarred"
 
 G_END_DECLS
 
