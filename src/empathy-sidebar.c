131b0099496470abddae9abda7fb50fedf6b4910
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2010-03-17 14:48:12 +0100

    sidebar: fix assertion in empathy_sidebar_menu_position_under
    For some reason this assertion was done the wrong way. I checked in eog and
    it's supposed to be done that way (#612557).

--- src/empathy-sidebar.c
+++ src/empathy-sidebar.c
@@ -241,7 +241,7 @@ empathy_sidebar_menu_position_under (GtkMenu *menu,
   GtkAllocation allocation;
 
   g_return_if_fail (GTK_IS_BUTTON (user_data));
-  g_return_if_fail (gtk_widget_get_has_window (user_data));
+  g_return_if_fail (!gtk_widget_get_has_window (user_data));
 
   widget = GTK_WIDGET (user_data);
 
