04d48c09e75845e6ca99b591f1821452667ba770
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2010-05-04 18:11:54 +0200

    release.py: update last_tag_patern

--- release.py
+++ release.py
@@ -9,7 +9,7 @@ import time
 from string import Template
 from optparse import OptionParser
 
-last_tag_patern = 'EMPATHY_2_30*'
+last_tag_patern = 'EMPATHY_2_31*'
 upload_server = 'master.gnome.org'
 template = '''\
 $name $version is now available for download from:
