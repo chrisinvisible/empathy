e7f7ba7259d1b668b0d1702687f7241581b74d87
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2010-06-21 10:32:28 +0200

    strip name of the room (#622201)

--- src/empathy-new-chatroom-dialog.c
+++ src/empathy-new-chatroom-dialog.c
@@ -731,6 +731,8 @@ new_chatroom_dialog_join (EmpathyNewChatroomDialog *dialog)
 		room_name = g_strdup (room);
 	}
 
+	g_strstrip (room_name);
+
 	DEBUG ("Requesting channel for '%s'", room_name);
 	empathy_dispatcher_join_muc (connection, room_name,
 		gtk_get_current_event_time (), NULL, NULL);
