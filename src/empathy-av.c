53fa331b0f2ad50a8520c4adf6983b744d5cddbd
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2010-06-24 15:47:33 +0200

    Add empathy-av, a new process handling media channels (#599161)

---
+++ src/empathy-av.c
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2007-2010 Collabora Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ *
+ * Authors: Xavier Claessens <xclaesse@gmail.com>
+ *          Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
+ */
+
+#include <config.h>
+
+#include <glib.h>
+#include <glib/gi18n.h>
+#include <gtk/gtk.h>
+
+#include <telepathy-glib/debug-sender.h>
+
+#include <libempathy/empathy-call-factory.h>
+#include <libempathy-gtk/empathy-ui-utils.h>
+
+#include "empathy-call-window.h"
+
+#define DEBUG_FLAG EMPATHY_DEBUG_VOIP
+#include <libempathy/empathy-debug.h>
+
+#include <gst/gst.h>
+
+static void
+new_call_handler_cb (EmpathyCallFactory *factory,
+    EmpathyCallHandler *handler,
+    gboolean outgoing,
+    gpointer user_data)
+{
+  EmpathyCallWindow *window;
+
+  window = empathy_call_window_new (handler);
+  gtk_widget_show (GTK_WIDGET (window));
+}
+
+int
+main (int argc,
+    char *argv[])
+{
+  GOptionContext *optcontext;
+  GOptionEntry options[] = {
+      { NULL }
+  };
+#ifdef ENABLE_DEBUG
+  TpDebugSender *debug_sender;
+#endif
+  EmpathyCallFactory *call_factory;
+
+  /* Init */
+  g_thread_init (NULL);
+
+  optcontext = g_option_context_new (N_("- Empathy Audio/Video Client"));
+  g_option_context_add_group (optcontext, gst_init_get_option_group ());
+  g_option_context_add_group (optcontext, gtk_get_option_group (TRUE));
+  g_option_context_add_main_entries (optcontext, options, GETTEXT_PACKAGE);
+
+  if (!g_option_context_parse (optcontext, &argc, &argv, &error)) {
+    g_print ("%s\nRun '%s --help' to see a full list of available command "
+        "line options.\n",
+        error->message, argv[0]);
+    g_warning ("Error in empathy-av init: %s", error->message);
+    return EXIT_FAILURE;
+  }
+
+  g_option_context_free (optcontext);
+
+  empathy_gtk_init ();
+  g_set_application_name (_("Empathy Audio/Video Client"));
+  g_setenv ("PULSE_PROP_media.role", "phone", TRUE);
+
+  gtk_window_set_default_icon_name ("empathy");
+  textdomain (GETTEXT_PACKAGE);
+
+#ifdef ENABLE_DEBUG
+  /* Set up debug sender */
+  debug_sender = tp_debug_sender_dup ();
+  g_log_set_default_handler (tp_debug_sender_log_handler, G_LOG_DOMAIN);
+#endif
+
+  call_factory = empathy_call_factory_initialise ();
+
+  g_signal_connect (G_OBJECT (call_factory), "new-call-handler",
+      G_CALLBACK (new_call_handler_cb), NULL);
+
+  gtk_main ();
+
+  g_object_unref (call_factory);
+
+#ifdef ENABLE_DEBUG
+  g_object_unref (debug_sender);
+#endif
+
+  return EXIT_SUCCESS;
+}
