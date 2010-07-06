c17ffb62ebc6e69071e0a0380b2402ba8201bb83
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2010-07-06 11:23:46 +0200

    dispatcher_channel_invalidated_cb: always remove invalidated chan from the list
    It doesn't hurt to always try to remove it and prevent us of keeping an
    invalidated or disposed object around if something weird happens.

--- libempathy/empathy-dispatcher.c
+++ libempathy/empathy-dispatcher.c
@@ -414,6 +414,8 @@ dispatcher_channel_invalidated_cb (TpProxy *proxy,
 
   connection = tp_channel_borrow_connection (TP_CHANNEL (proxy));
 
+  priv->channels = g_list_remove (priv->channels, proxy);
+
   cd = g_hash_table_lookup (priv->connections, connection);
   /* Connection itself invalidated? */
   if (cd == NULL)
@@ -425,8 +427,6 @@ dispatcher_channel_invalidated_cb (TpProxy *proxy,
 
   g_hash_table_remove (cd->dispatched_channels, object_path);
   g_hash_table_remove (cd->dispatching_channels, object_path);
-
-  priv->channels = g_list_remove (priv->channels, proxy);
 }
 
 static void
