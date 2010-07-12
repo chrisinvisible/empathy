268f91e2666922f1a7ced025d0a69956a7257e73
Cosimo Cecchi <cosimoc@gnome.org>
2010-07-12 19:32:39 +0200

    Add a debug flag for TLS

--- libempathy/empathy-debug.h
+++ libempathy/empathy-debug.h
@@ -46,6 +46,7 @@ typedef enum
   EMPATHY_DEBUG_IMPORT_MC4_ACCOUNTS = 1 << 11,
   EMPATHY_DEBUG_TESTS = 1 << 12,
   EMPATHY_DEBUG_VOIP = 1 << 13,
+  EMPATHY_DEBUG_TLS = 1 << 14,
 } EmpathyDebugFlags;
 
 gboolean empathy_debug_flag_is_set (EmpathyDebugFlags flag);
